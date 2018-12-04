fc0dbbff3d91507ba127f314625d12f5e453fd86
havuong <havuong138@gmail.com>
2018-12-04 19:01:09 +0700

    [Basic JavaScript] Compound Assignment With Augmented Subtraction

--- basics/compound-assignment-with-augmented.js
+++ basics/compound-assignment-with-augmented.js
@@ -6,3 +6,12 @@ var c = 12;
 a += 12;
 b += 9;
 c += 7;
+
+// Compound Assignment With Augmented Subtraction
+var a = 11;
+var b = 9;
+var c = 3;
+
+a -= 6; // a = 5 
+b -= 15; // b = -6
+c -= 1; // c = 2
