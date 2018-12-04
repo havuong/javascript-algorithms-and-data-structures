42b3ad1052d84825f8995915cccc0301d07271d4
havuong <havuong138@gmail.com>
2018-12-04 20:02:30 +0700

    [Basic JavaScript] Compound Assignment With Augmented Multiplication

--- basics/compound-assignment-with-augmented.js
+++ basics/compound-assignment-with-augmented.js
@@ -15,3 +15,12 @@ var c = 3;
 a -= 6; // a = 5 
 b -= 15; // b = -6
 c -= 1; // c = 2
+
+// Compound Assignment With Augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5; // a = 25
+b *= 3; // b = 36
+c *= 10; // c = 46
