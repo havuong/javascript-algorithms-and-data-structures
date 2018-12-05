bde5aea6acb6d26902bb3a97cbdd8b51d725bd3c
havuong <havuong138@gmail.com>
2018-12-05 20:50:14 +0700

    [Basic JavaScript] Escape Sequences in Strings

--- basics/declare-string-variables.js
+++ basics/declare-string-variables.js
@@ -7,3 +7,6 @@ var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 
 // Quoting Strings with Single Quotes
 var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// Escape Sequences in Strings
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
