66f2c203444306588879490187272ad5a49d1f37
havuong <havuong138@gmail.com>
2018-12-05 19:40:33 +0700

    [Basic JavaScript] Escaping Literal Quotes in Strings

--- basics/declare-string-variables.js
+++ basics/declare-string-variables.js
@@ -1,3 +1,6 @@
 // Declare String Variables
 var myFirstName = "a"
 var myLastName = "b"
+
+// Escaping Literal Quotes in Strings
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
