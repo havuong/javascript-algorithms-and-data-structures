afacf3b035d1019a33efc8bc2cf0d4eb663bcca9
havuong <havuong138@gmail.com>
2018-12-05 20:20:42 +0700

    [Basic JavaScript] Quoting Strings with Single Quotes

--- basics/declare-string-variables.js
+++ basics/declare-string-variables.js
@@ -4,3 +4,6 @@ var myLastName = "b"
 
 // Escaping Literal Quotes in Strings
 var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// Quoting Strings with Single Quotes
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
