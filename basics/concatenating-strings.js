5c852848ac542d64529e28d521db6421b49394e5
havuong <havuong138@gmail.com>
2018-12-06 19:52:10 +0700

    [Basic JavaScript] Concatenating Strings with the Plus Equals Operator

--- basics/concatenating-strings.js
+++ basics/concatenating-strings.js
@@ -1,3 +1,8 @@
 // Concatenating Strings with Plus Operator
 
 var myStr = "This is the start. " + "This is the end.";
+
+// Concatenating Strings with the Plus Equals Operator
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
