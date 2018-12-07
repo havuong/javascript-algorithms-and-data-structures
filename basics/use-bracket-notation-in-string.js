0d8fe3ffb47f289f9800d566b353e49b20e0d19d
havuong <havuong138@gmail.com>
2018-12-07 19:58:47 +0700

    [Basic JavaScript] Use Bracket Notation to Find the First Character in a String

--- basics/use-bracket-notation-in-string.js
+++ basics/use-bracket-notation-in-string.js
@@ -1,6 +1,11 @@
 // Find the Length of a String
-
 var lastNameLength = 0;
 var lastName = "Lovelace";
 
 lastNameLength = lastName.length;
+
+// Use Bracket Notation to Find the First Character in a String
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
