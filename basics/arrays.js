a4d66ee0e0cf66be8bdebc5603ce7d1417457ab2
havuong <havuong138@gmail.com>
2018-12-08 21:09:18 +0700

    [Basic JavaScript] Modify Array Data With Indexes

--- basics/arrays.js
+++ basics/arrays.js
@@ -8,3 +8,8 @@ var myArray = [["the universe", 42], ["everything", 101010]];
 var myArray = [50,60,70];
 
 var myData = myArray[0]; // equals 50
+
+// Modify Array Data With Indexes
+var myArray = [18,64,99];
+
+myArray[0] = 45; // ourArray now equals [45,64,99].
