b7d98976db613823256cbc3053edc495642cc8a8
havuong <havuong138@gmail.com>
2018-12-08 20:25:16 +0700

    [Basic JavaScript] Nest one Array within Another Array

--- basics/arrays.js
+++ basics/arrays.js
@@ -1,2 +1,5 @@
 // Store Multiple Values in one Variable using JavaScript Arrays
 var myArray = ["Ha",10];
+
+// Nest one Array within Another Array
+var myArray = [["the universe", 42], ["everything", 101010]];
