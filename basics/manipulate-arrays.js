95e047b6a4be5b9847dde9e8fdc09701623205c2
havuong <havuong138@gmail.com>
2018-12-10 18:27:23 +0700

    [Basic JavaScript] Manipulate Arrays With push()

---
+++ basics/manipulate-arrays.js
@@ -0,0 +1,6 @@
+// Manipulate Arrays With push()
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); // ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]); // myArray now equals [["John", 23], ["cat", 2], ["dog", 3]]
