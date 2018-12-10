a2f73d3179def45ce4d750014556615682094baf
havuong <havuong138@gmail.com>
2018-12-10 18:58:37 +0700

    [Basic JavaScript] Manipulate Arrays With pop()

--- basics/manipulate-arrays.js
+++ basics/manipulate-arrays.js
@@ -4,3 +4,10 @@ ourArray.push(["happy", "joy"]); // ourArray now equals ["Stimpson", "J", "cat",
 
 var myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3]); // myArray now equals [["John", 23], ["cat", 2], ["dog", 3]]
+
+// Manipulate Arrays With pop()
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop(); // removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+var myArray = [["John", 23], ["cat", 2]];
+var removedFromMyArray = myArray.pop();
