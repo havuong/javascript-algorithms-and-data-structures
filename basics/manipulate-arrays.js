d8b9912729b853f2cd48e701335d3705044f0c53
havuong <havuong138@gmail.com>
2018-12-10 19:39:09 +0700

    [Basic JavaScript] Manipulate Arrays With unshift()

--- basics/manipulate-arrays.js
+++ basics/manipulate-arrays.js
@@ -18,3 +18,12 @@ var removedFromOurArray = ourArray.shift(); // removedFromOurArray now equals "S
 
 var myArray = [["John", 23], ["dog", 3]];
 var removedFromMyArray = myArray.shift();
+
+// Manipulate Arrays With unshift()
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); // ourArray now equals ["Happy", "J", "cat"]
+
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
