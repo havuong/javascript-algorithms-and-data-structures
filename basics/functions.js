d48b15bb7d2b3583a3dd7b4947ff7e1a919c0a2b
havuong <havuong138@gmail.com>
2018-12-12 19:59:50 +0700

    [Basic JavaScript] Passing Values to Functions with Arguments

--- basics/functions.js
+++ basics/functions.js
@@ -4,3 +4,11 @@ function reusableFunction(){
 }
 
 reusableFunction();
+
+// Passing Values to Functions with Arguments
+function functionWithArgs(param1, param2) {
+  console.log(param1 + param2);
+}
+
+functionWithArgs(1,2);
+functionWithArgs(7,9);
