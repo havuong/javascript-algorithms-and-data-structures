d261a6b725d15b0cd60db8e35e9733e3d8fb8cad
havuong <havuong138@gmail.com>
2018-12-13 19:02:07 +0700

    [Basic JavaScript] Global Scope and Functions

---
+++ basics/scope-and-functions.js
@@ -0,0 +1,18 @@
+// Global Scope and Functions
+var myGlobal = 10;
+var oopsGlobal;
+function fun1() {
+  oopsGlobal = 5;
+  
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
