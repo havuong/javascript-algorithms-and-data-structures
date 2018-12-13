62bbb6740809d836d2f0216b48ee461c68e2a212
havuong <havuong138@gmail.com>
2018-12-13 20:04:52 +0700

    [Basic JavaScript] Local Scope and Functions

--- basics/scope-and-functions.js
+++ basics/scope-and-functions.js
@@ -16,3 +16,12 @@ function fun2() {
   }
   console.log(output);
 }
+
+// Local Scope and Functions
+function myLocalScope() {
+  'use strict';
+  var myVar;
+  console.log(myVar);
+}
+myLocalScope(); // Run and check the console // myVar is not defined outside of myLocalScope
+console.log(myVar); // Now remove the console log line to pass the test
