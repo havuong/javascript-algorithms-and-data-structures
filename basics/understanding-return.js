4fd6a470db1df5ab8695284d123ab1843299cf48
havuong <havuong138@gmail.com>
2018-12-15 19:27:33 +0700

    [Basic JavaScript] Return a Value from a Function with Return

---
+++ basics/understanding-return.js
@@ -0,0 +1,13 @@
+// Return a Value from a Function with Return
+function minusSeven(num) {
+  return num - 7;
+}
+
+function timesFive(num){
+  return num *= 5;
+}
+timesFive(5);
+timesFive(2);
+timesFive(0);
+
+console.log(minusSeven(10));
