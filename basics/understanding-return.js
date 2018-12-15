f2c463da2557d2cdac1d359754f90ef1c70ad27f
havuong <havuong138@gmail.com>
2018-12-15 21:15:07 +0700

    [Basic JavaScript] Assignment with a Returned Value

--- basics/understanding-return.js
+++ basics/understanding-return.js
@@ -1,10 +1,10 @@
 // Return a Value from a Function with Return
 function minusSeven(num) {
-  return num - 7;
+    return num - 7;
 }
 
-function timesFive(num){
-  return num *= 5;
+function timesFive(num) {
+    return num *= 5;
 }
 timesFive(5);
 timesFive(2);
@@ -15,11 +15,28 @@ console.log(minusSeven(10));
 // Understanding Undefined Value returned from a Function
 var sum = 0;
 function addThree() {
-  sum = sum + 3;
+    sum = sum + 3;
 }
 
-function addFive(){
-  sum += 5;
+function addFive() {
+    sum += 5;
 }
 
 var returnedValue = addFive();
+
+// Assignment with a Returned Value
+var changed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+}
+
+changed = change(10);
+
+var processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
