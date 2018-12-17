10b8646a25474d904c9faaf533e1df7b6ba8bdbc
havuong <havuong138@gmail.com>
2018-12-17 20:13:49 +0700

    [Basic JavaScript] Use Conditional Logic with If Statements

--- basics/understanding-boolean.js
+++ basics/understanding-boolean.js
@@ -2,3 +2,13 @@
 function welcomeToBooleans() {
     return true;
 }
+
+// Use Conditional Logic with If Statements
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+}
+
+trueOrFalse(true);
