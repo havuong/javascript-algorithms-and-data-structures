77b1617e978bb7c4378ec49ffa88de753a264b42
havuong <havuong138@gmail.com>
2018-12-18 22:59:13 +0700

    [Basic JavaScript] Comparison with the Strict Inequality Operator

--- basics/comparison.js
+++ basics/comparison.js
@@ -37,3 +37,13 @@ function testNotEqual(val) {
 }
 
 testNotEqual(10);
+
+// Comparison with the Strict Inequality Operator
+function testStrictNotEqual(val) {
+    if (val !== 17) {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+testStrictNotEqual(10);
