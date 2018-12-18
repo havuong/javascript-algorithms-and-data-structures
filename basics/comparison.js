fc933bc54f4f4beb2ffeccf8dca7063910d1a170
havuong <havuong138@gmail.com>
2018-12-18 22:06:45 +0700

    [Basic JavaScript] Practice comparing different values

--- basics/comparison.js
+++ basics/comparison.js
@@ -17,3 +17,13 @@ function testStrict(val) {
 }
 
 testStrict(10);
+
+// Practice comparing different values
+function compareEquality(a, b) {
+    if (a === b) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+compareEquality(10, "10");
