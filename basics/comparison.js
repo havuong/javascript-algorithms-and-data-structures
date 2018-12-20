d6729927e9de84ddf7a022eb8c12923d37f005ca
havuong <havuong138@gmail.com>
2018-12-20 21:41:47 +0700

    [Basic JavaScript] Comparisons with the Logical Or Operator

--- basics/comparison.js
+++ basics/comparison.js
@@ -117,3 +117,13 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+
+// Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+    if (val < 10 || val > 20) {
+        return "Outside";
+    }
+    return "Inside";
+}
+
+testLogicalOr(15);
