ae6740ac8ae6ea0bc4d970bde621e27bd83ef01d
havuong <havuong138@gmail.com>
2018-12-20 20:50:14 +0700

    [Basic JavaScript] Comparisons with the Logical And Operator

--- basics/comparison.js
+++ basics/comparison.js
@@ -107,3 +107,13 @@ function testLessOrEqual(val) {
 }
 
 testLessOrEqual(10);
+
+// Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+    }
+    return "No";
+}
+
+testLogicalAnd(10);
