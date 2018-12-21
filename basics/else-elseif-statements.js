b719071b182186426fc1df77ba52a524306824a2
havuong <havuong138@gmail.com>
2018-12-21 20:54:34 +0700

    [Basic JavaScript] Chaining If Else Statements

--- basics/else-elseif-statements.js
+++ basics/else-elseif-statements.js
@@ -38,3 +38,20 @@ function orderMyLogic(val) {
 }
 
 orderMyLogic(4);
+
+// Chaining If Else Statements
+function testSize(num) {
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
+    } else {
+        return "Huge";
+    }
+}
+
+testSize(7);
