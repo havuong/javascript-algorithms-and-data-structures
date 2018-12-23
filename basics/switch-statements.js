00998ae880e72e84321d7142d9d8fb03f8f46b1d
havuong <havuong138@gmail.com>
2018-12-23 19:36:59 +0700

    [Basic JavaScript] Multiple Identical Options in Switch Statements

--- basics/switch-statements.js
+++ basics/switch-statements.js
@@ -41,3 +41,28 @@ function switchOfStuff(val) {
 }
 
 switchOfStuff(1);
+
+// Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+    var answer = "";
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+    return answer;
+}
+
+sequentialSizes(1);
