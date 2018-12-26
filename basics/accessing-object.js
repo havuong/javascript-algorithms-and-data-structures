ce55ec976feae96cb998481d698388db4ccad1e1
havuong <havuong138@gmail.com>
2018-12-26 19:12:58 +0700

    [Basic JavaScript] Build JavaScript Objects

---
+++ basics/accessing-object.js
@@ -0,0 +1,13 @@
+// Build JavaScript Objects
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+var myDog = {
+    "name": "lulu",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["mimi", "kiki", "sasa"]
+};
