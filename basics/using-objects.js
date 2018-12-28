fcc9a661eab0b845ef87edceded82b4644736f9c
havuong <havuong138@gmail.com>
2018-12-28 19:06:12 +0700

    [Basic JavaScript] Manipulating Complex Objects

--- basics/using-objects.js
+++ basics/using-objects.js
@@ -32,3 +32,27 @@ function checkObj(checkProp) {
 }
 
 checkObj("gift");
+
+// Manipulating Complex Objects
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "My Tam",
+        "title": "Nguoi hay quen em di",
+        "release_year": 2015,
+        "formats": [
+            "CD",
+            "DVD"
+        ]
+    }
+];
