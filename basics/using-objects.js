a442db22070ca85126acd70ee06021c7f17fd921
havuong <havuong138@gmail.com>
2018-12-28 20:19:18 +0700

    [Basic JavaScript] Accessing Nested Arrays

--- basics/using-objects.js
+++ basics/using-objects.js
@@ -71,3 +71,25 @@ var myStorage = {
 };
 
 var gloveBoxContents = myStorage.car.inside["glove box"];
+
+// Accessing Nested Arrays
+var myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+var secondTree = myPlants[1].list[1];
