a3c995e3c71904d811dc05c54b28db1df469c1d4
havuong <havuong138@gmail.com>
2019-01-03 20:31:30 +0700

    [Basic JavaScript] Generate Random Whole Numbers within a Range

--- basics/generate-random.js
+++ basics/generate-random.js
@@ -9,3 +9,10 @@ var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
 function randomWholeNum() {
     return Math.floor(Math.random() * 10);
 }
+
+// Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+
+var myRandom = randomRange(5, 15);
