80c8f31d995b9ccc4c8a17513aaa618cd7c474c3
havuong <havuong138@gmail.com>
2019-01-03 20:05:42 +0700

    [Basic JavaScript] Generate Random Whole Numbers with JavaScript

--- basics/generate-random.js
+++ basics/generate-random.js
@@ -2,3 +2,10 @@
 function randomFraction() {
     return Math.random();
 }
+
+// Generate Random Whole Numbers with JavaScript
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
