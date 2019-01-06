46cf0d4c5bb936de2ce7f17bac82e7900a7d38d8
havuong <havuong138@gmail.com>
2019-01-06 15:50:44 +0700

    [ES6] Mutate an Array Declared with const

--- es6/const-keyword.js
+++ es6/const-keyword.js
@@ -19,3 +19,14 @@ function printManyTimes(str) {
     }
 }
 printManyTimes("freeCodeCamp");
+
+// Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+    "use strict";
+    // s = [2, 5, 7]; <- this is invalid
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+editInPlace();
