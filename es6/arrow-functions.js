b75c878159f13ad584f0afc917f64a14913aed21
havuong <havuong138@gmail.com>
2019-01-07 19:38:55 +0700

    [ES6] Write Higher Order Arrow Functions

--- es6/arrow-functions.js
+++ es6/arrow-functions.js
@@ -14,3 +14,17 @@ const magic = () => new Date();
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+// Write Higher Order Arrow Functions
+// Use arrow function syntax to compute the square of only the positive integers 
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+    "use strict";
+
+    const squaredIntegers = arr.filter((num) => num > 0 && num % parseInt(num) === 0).map((num) => Math.pow(num, 2));
+
+    return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
