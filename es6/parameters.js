d54ed4944e55b0ea5a31016a8b71ab438db1ee85
havuong <havuong138@gmail.com>
2019-01-09 18:58:41 +0700

    [ES6] Set Default Parameters for Your Functions

---
+++ es6/parameters.js
@@ -0,0 +1,8 @@
+// Set Default Parameters for Your Functions
+const increment = (function () {
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
