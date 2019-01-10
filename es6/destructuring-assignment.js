bee7441751c48475d0120e5109ffa4417216ed21
havuong <havuong138@gmail.com>
2019-01-10 21:02:16 +0700

    [ES6] Use Destructuring Assignment to Assign Variables from Nested Objects

--- es6/destructuring-assignment.js
+++ es6/destructuring-assignment.js
@@ -21,3 +21,17 @@ function getTempOfTmrw(avgTemperatures) {
 }
 
 console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
+
+// Use Destructuring Assignment to Assign Variables from Nested Objects
+
+const LOCAL_FORECAST = {
+    today: { min: 72, max: 83 },
+    tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+    const { tomorrow: { max: maxOfTomorrow } } = forecast;
+    return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
