70bfa1a41f5f2f2942452e877b24328a5988a435
havuong <havuong138@gmail.com>
2019-01-11 19:51:57 +0700

    [ES6] Use Destructuring Assignment to Pass an Object as a Function's Parameters

--- es6/destructuring-assignment.js
+++ es6/destructuring-assignment.js
@@ -53,3 +53,23 @@ function removeFirstTwo(list) {
 const arr = removeFirstTwo(source);
 console.log(arr); // should be [3,4,5,6,7,8,9,10]
 console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function () {
+    "use strict";
+    return function half({ max, min }) {
+        // use function argument destructuring
+        return (max + min) / 2.0;
+    };
+
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
