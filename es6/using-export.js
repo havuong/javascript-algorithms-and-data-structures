2c6576436af6d1d1880dfd857679597d57213de8
havuong <havuong138@gmail.com>
2019-01-13 15:25:28 +0700

    [ES6] Use export to Reuse a Code Block

---
+++ es6/using-export.js
@@ -0,0 +1,10 @@
+// Use export to Reuse a Code Block
+/*
+We must utilize one of the statements that goes with import, known as export.
+When we want some code - a function, or a variable - to be usable in another file,
+we must export it in order to import it into another file.
+Like import, export is a non-browser feature.
+*/
+
+export { capitalizeString } //How to export functions.
+export const foo = "bar"; //How to export variables.
