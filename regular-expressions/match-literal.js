4e0707c38c25e23f0e1a46748175f79612fd9bf1
havuong <havuong138@gmail.com>
2019-01-16 16:10:39 +0700

    [Regular Expressions] Find More Than the First Match

--- regular-expressions/match-literal.js
+++ regular-expressions/match-literal.js
@@ -19,3 +19,9 @@ let result = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
 let result = extractStr.match(codingRegex); // Returns ["/coding/; "]
+
+// Find More Than the First Match
+// To search or extract a pattern more than once, you can use the "g" flag.
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/ig;
+let result = twinkleStar.match(starRegex);
