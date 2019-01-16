362c86f3d0cea4362e4877d4ad7e22a1964f02ff
havuong <havuong138@gmail.com>
2019-01-16 13:13:35 +0700

    [Regular Expressions] Match a Literal String with Different Possibilities

--- regular-expressions/match-literal.js
+++ regular-expressions/match-literal.js
@@ -3,3 +3,8 @@ let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // true
 let waldoRegex = /waldo/; // true
 let result = waldoRegex.test(waldoIsHiding);
+
+// Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let result = petRegex.test(petString);
