bd893f3b95d506ccf341aaacf83d1e34bd8d4478
havuong <havuong138@gmail.com>
2019-01-18 16:58:37 +0700

    [Regular Expressions] Match Numbers and Letters of the Alphabet

--- regular-expressions/match-literal.js
+++ regular-expressions/match-literal.js
@@ -54,3 +54,10 @@ let result = quoteSample.match(vowelRegex);
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/ig;
 let result = quoteSample.match(alphabetRegex);
+
+// Match Numbers and Letters of the Alphabet
+// Using the hyphen (-) to match a range of characters is not limited to letters.
+// It also works to match a range of numbers.
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig;
+let result = quoteSample.match(myRegex);
