37079b7d6c2694f7f96ecb37180757b087243027
havuong <havuong138@gmail.com>
2019-01-21 18:29:31 +0700

    [Regular Expressions] Match All Letters and Numbers

--- regular-expressions/match-literal.js
+++ regular-expressions/match-literal.js
@@ -117,3 +117,21 @@ let result = calRegex.test(rickyAndCal);
 let caboose = "The last car on a train is the caboose";
 let lastRegex = /caboose$/;
 let result = lastRegex.test(caboose);
+
+// Match All Letters and Numbers
+// Using character classes, you were able to search for all letters of the alphabet with [a-z].
+// The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_].
+// This character class matches upper and lowercase letters plus numbers.
+// Note, this character class also includes the underscore character (_).
+let longHand = /[A-Za-z0-9_]+/;
+let shortHand = /\w+/;
+let numbers = "42";
+let varNames = "important_var";
+longHand.test(numbers); // Returns true
+shortHand.test(numbers); // Returns true
+longHand.test(varNames); // Returns true
+shortHand.test(varNames); // Returns true
+// These shortcut character classes are also known as shorthand character classes.
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g;
+let result = quoteSample.match(alphabetRegexV2).length;
