de99bff50c8acd954fb49c8f400e38cdeed82728
havuong <havuong138@gmail.com>
2019-01-21 20:03:21 +0700

    [Regular Expressions] Match All Numbers

--- regular-expressions/match-literal.js
+++ regular-expressions/match-literal.js
@@ -151,3 +151,11 @@ sentence.match(shortHand); // Returns ["!"]
 let quoteSample = "The five boxing wizards jump quickly.";
 let nonAlphabetRegex = /\W/g;
 let result = quoteSample.match(nonAlphabetRegex).length;
+
+// Match All Numbers
+// The shortcut to look for digit characters is \d, with a lowercase d.
+// This is equal to the character class [0-9],
+// which looks for a single character of any number between zero and nine.
+let numString = "Your sandwich will be $5.00";
+let numRegex = /\d/g;
+let result = numString.match(numRegex).length;
