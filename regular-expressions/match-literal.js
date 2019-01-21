5e18e588e19b815e74e10ae285814777f0413e51
havuong <havuong138@gmail.com>
2019-01-21 20:54:02 +0700

    [Regular Expressions] Match All Non-Numbers

--- regular-expressions/match-literal.js
+++ regular-expressions/match-literal.js
@@ -159,3 +159,11 @@ let result = quoteSample.match(nonAlphabetRegex).length;
 let numString = "Your sandwich will be $5.00";
 let numRegex = /\d/g;
 let result = numString.match(numRegex).length;
+
+// Match All Non-Numbers
+// The shortcut to look for non-digit characters is \D.
+// This is equal to the character class [^0-9],
+// which looks for a single character that is not a number between zero and nine
+let numString = "Your sandwich will be $5.00";
+let noNumRegex = /\D/g;
+let result = numString.match(noNumRegex).length;
