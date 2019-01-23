81d6ab6d504628d84aa5a56b3214f6a63c98e7af
havuong <havuong138@gmail.com>
2019-01-23 22:05:03 +0700

    [Regular Expressions] Check for All or None

--- regular-expressions/match-literal.js
+++ regular-expressions/match-literal.js
@@ -209,3 +209,15 @@ let result = haRegex.test(haStr);
 let timStr = "Timmmmber";
 let timRegex = /Tim{4}ber/;
 let result = timRegex.test(timStr)
+
+// Check for All or None
+/*
+Sometimes the patterns you want to search for may have parts of it that may or may not exist.
+However, it may be important to check for them nonetheless.
+You can specify the possible existence of an element with a question mark, ?.
+This checks for zero or one of the preceding element.
+You can think of this symbol as saying the previous element is optional.
+*/
+let favWord = "favorite";
+let favRegex = /favou?rite/;
+let result = favRegex.test(favWord);
