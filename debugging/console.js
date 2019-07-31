049b4da4044765bf13cc516e64c3a92dd0246d7d
havuong <havuong138@gmail.com>
2019-07-31 13:41:56 +0700

    [Debugging] Understanding the Differences between the freeCodeCamp and Browser Console

--- debugging/console.js
+++ debugging/console.js
@@ -5,3 +5,10 @@ a++;
 console.log(a);
 let sumAB = a + b;
 console.log(sumAB);
+
+// Understanding the Differences between the freeCodeCamp and Browser Console
+let outputTwo = "This will print to the browser console 2 times";
+console.log(outputTwo);
+let outputOne = "Try to get this to log only once to the browser console";
+console.clear(); // Use console.clear() in the next line to print the outputOne only once
+console.log(outputOne);
