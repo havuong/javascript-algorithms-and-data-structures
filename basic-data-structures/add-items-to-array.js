71e55dbc232c29d9c70459a56644538f13a78dc2
havuong <havuong138@gmail.com>
2019-08-04 15:35:41 +0700

    [Basic Data Structures] Add Items Using splice()

--- basic-data-structures/add-items-to-array.js
+++ basic-data-structures/add-items-to-array.js
@@ -6,3 +6,12 @@ function mixedNumbers(arr) {
 }
 
 console.log(mixedNumbers(['IV', 5, 'six']));
+
+// Add Items Using splice()
+// use that third parameter, which represents one or more elements, to add them as well
+function htmlColorNames(arr) {
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond');
+    return arr;
+}
+// DarkSalmon,BlanchedAlmond,LavenderBlush,PaleTurqoise,FireBrick
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
