09e61814401e6fbf61e2f9d21980bfdf3be7080f
havuong <havuong138@gmail.com>
2019-08-08 14:43:16 +0700

    [Basic Data Structures] Generate an Array of All Object Keys with Object.keys()

--- basic-data-structures/interact-an-object.js
+++ basic-data-structures/interact-an-object.js
@@ -87,3 +87,29 @@ function countOnline(obj) {
 }
 
 console.log(countOnline(users));
+
+// Generate an Array of All Object Keys with Object.keys()
+let users = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function getArrayOfUsers(obj) {
+    return Object.keys(obj);
+}
+
+console.log(getArrayOfUsers(users));
