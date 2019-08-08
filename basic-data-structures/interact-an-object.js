6c0b47b8e8efa4205a5c5e535b38b491c9f9eb49
havuong <havuong138@gmail.com>
2019-08-08 13:29:30 +0700

    [Basic Data Structures] Iterate Through the Keys of an Object with a for...in Statement

--- basic-data-structures/interact-an-object.js
+++ basic-data-structures/interact-an-object.js
@@ -55,3 +55,35 @@ function isEveryoneHere(obj) {
 }
 
 console.log(isEveryoneHere(users));
+
+// Iterate Through the Keys of an Object with a for...in Statement
+let users = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function countOnline(obj) {
+    let result = 0;
+    for (let user in obj) {
+        if (obj[user].online === true) {
+            result++;
+        };
+    }
+    return result;
+}
+
+console.log(countOnline(users));
